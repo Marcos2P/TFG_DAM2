80270f641faefed3ff81adfc67a237239e444f93
mpedreira2023 <m.pedreira@spmgm.com>
2023-04-26 16:55:18 +0200

    Comienzo con los fragments

--- app/src/main/java/com/example/tfg_dam2/actividades_principales/PrincipalActivity.kt
+++ app/src/main/java/com/example/tfg_dam2/actividades_principales/PrincipalActivity.kt
@@ -2,11 +2,22 @@ package com.example.tfg_dam2.actividades_principales
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.example.tfg_dam2.R
+import com.example.tfg_dam2.clases_alternativas.MainFragment
 
 class PrincipalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
+
+        //Colocar Fragmetns
+        supportFragmentManager.commit {
+            replace<MainFragment>(R.id.frameContainer)
+            setReorderingAllowed(true)
+            addToBackStack("replacement")
+        }
+
     }
 }
